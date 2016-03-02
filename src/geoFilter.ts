5c80277495f091d67fafc15066ad2204cb548518
Jiayu Liu <jiayu@MacBook-Pro.local>
2016-03-02 18:49:54 +0800

    adding support for layer group

--- src/geoFilter.ts
+++ src/geoFilter.ts
@@ -1,4 +1,5 @@
 /// <reference path="./geolib.d.ts" />
+
 import { Point, isPointInCircle, isPointInside } from "geolib";
 
 export interface LatLngFilter {
@@ -12,17 +13,41 @@ function convertPoint(latLng: L.LatLng): Point {
   };
 }
 
-export function leafletLayerToLatLngFilter(layer: L.ILayer, layerType: string): LatLngFilter {
+function getLayerType(layer: L.ILayer): string {
+  if (layer instanceof L.Circle) {
+    return "circle";
+  } else if (layer instanceof L.Polygon) {
+    if (layer instanceof L.Rectangle) {
+      return "rectangle";
+    } else {
+      return "polygon";
+    }
+  } // else return undefined
+}
+
+export function leafletLayerToLatLngFilter(layer: L.ILayer, layerType: string = getLayerType(layer)): LatLngFilter {
   if (layerType === "circle") {
-    const circle: L.Circle = <L.Circle> layer;
+    const circle: L.Circle = <L.Circle>layer;
     return (latLng: L.LatLng) => isPointInCircle(convertPoint(latLng),
       convertPoint(circle.getLatLng()),
       circle.getRadius());
   } else if (layerType === "rectangle" || layerType === "polygon") {
-    const polygon: L.Polygon = <L.Polygon> layer;
-    return (latLng: L.LatLng) => isPointInside(convertPoint(latLng),
-      polygon.getLatLngs().map(convertPoint));
+    const polygon: L.Polygon = <L.Polygon>layer;
+    return (latLng: L.LatLng) => isPointInside(convertPoint(latLng), polygon.getLatLngs().map(convertPoint));
   } else {
-    return () => true;
+    // possibly polyline or marker, which should always return false
+    return () => false;
+  }
+}
+
+export function leafletLayerGroupToLatLngFilter<T extends L.ILayer>(layerGroup: L.LayerGroup<T>): LatLngFilter {
+  const predicates = layerGroup.getLayers().map(layer => leafletLayerToLatLngFilter(layer));
+  return (latLng: L.LatLng) => {
+    for (const predicate of predicates) {
+      if (predicate(latLng)) {
+        return true;
+      }
+    }
+    return false;
   }
 }
--- test/index.js
+++ test/index.js
@@ -5,27 +5,32 @@ const expect = chai.expect;
 
 describe('leaflet toolbelt', () => {
 
-  it('should work', () => {
+  it('should filter circle correctly', () => {
     const circle = {
       getLatLng() {
-        return {
-          lat: 0,
-          lng: 0
-        }
+        return { lat: 0, lng: 0 }
       },
       getRadius() {
         return 100;
       }
     };
     const f = LT.leafletLayerToLatLngFilter(circle, 'circle');
-    expect(f({
-      lat: 0,
-      lng: 0
-    })).to.be.equal(true);
-    expect(f({
-      lat: 101,
-      lng: 0
-    })).to.be.equal(false);
+    expect(f({ lat: 0, lng: 0 })).to.be.equal(true);
+    expect(f({ lat: 101, lng: 0 })).to.be.equal(false);
+  });
+
+  it('should filter polygon correctly', () => {
+    const polygon = {
+      getLatLngs() {
+        const p0 = { lat: 0, lng: 0, };
+        const p1 = { lat: 1, lng: 1, };
+        const p2 = { lat: 1, lng: 0, };
+        return [p0, p1, p2, p0];
+      }
+    };
+    const f = LT.leafletLayerToLatLngFilter(polygon, 'polygon');
+    expect(f({ lat: 0.5, lng: 0.5 })).to.be.equal(true);
+    expect(f({ lat: -1, lng: 0.5 })).to.be.equal(false);
   });
 
 });
